ee3f004a3f67a4322a497e52cfbc4c11430daa64
SUNILdebug <suniltoto31@gmail.com>
2026-09-15 22:59:03 +0100

    Update professor.js

--- professor.js
+++ professor.js
@@ -447,10 +447,3 @@ if ($("#refreshBtn")) $("#refreshBtn").onclick = loadDashboardData;
 
 setupModalClosers();
 checkAuth();
-window.switchTab = (tabId) => {
-  document.querySelectorAll(".tab-content").forEach(el => el.classList.add("hidden"));
-  document.querySelectorAll(".tab-btn").forEach(el => el.classList.remove("active"));
-  
-  document.getElementById(tabId)?.classList.remove("hidden");
-  event.target.classList.add("active");
-};
